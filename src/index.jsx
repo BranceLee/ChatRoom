30260c2d92ce6e8680f95b75f2cb06c9113e6820
BranceLee <475815059@qq.com>
2020-02-19 15:52:53 +0800

    project init

---
+++ src/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+const App = () => {
+  return <div>Hello Websocket</div>;
+};
+
+ReactDom.render(<App />, document.getElementById('_root'));
---
+++ webpack.config.js
@@ -0,0 +1,90 @@
+// webpack v3
+const path = require('path');
+
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const WebpackMd5Hash = require('webpack-md5-hash');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const autoprefixerPlugin = require('autoprefixer');
+
+module.exports = {
+  entry: ['@babel/polyfill', './src/index.jsx'],
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: '[name].[hash].js'
+  },
+
+  devServer: {
+    contentBase: './dist',
+    host: '0.0.0.0',
+    port: 3000,
+    hot: true
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env', '@babel/preset-react']
+            }
+          },
+          {
+            loader: 'eslint-loader'
+          }
+        ]
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          {
+            loader: 'postcss-loader',
+            options: {
+              plugins: [autoprefixerPlugin]
+            }
+          }
+        ]
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          {
+            loader: 'postcss-loader',
+            options: {
+              plugins: [autoprefixerPlugin]
+            }
+          },
+          'sass-loader'
+        ]
+      }
+    ]
+  },
+
+  plugins: [
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      filename: 'style.[contenthash].css'
+    }),
+    new HtmlWebpackPlugin({
+      inject: false,
+      hash: true,
+      template: './src/index.html',
+      filename: 'index.html',
+      minify: {
+        removeComments: true,
+        collapseWhitespace: true //删除空白符与换行符
+      }
+    }),
+    new WebpackMd5Hash()
+  ]
+};
